2bacfbcdb2845a62b7a5ea793f663f0e75a03af5
Gandhiert <136203533+Gandhiert@users.noreply.github.com>
2024-03-25 20:04:59 +0700

    Update awal.sh

--- soal_3/awal.sh
+++ soal_3/awal.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -O genshin.zip 'https://docs.google.com/uc?export=download&id=1oGHdTf4_76_RacfmQIV4i7os4sGwa9vN'
 unzip genshin.zip
 unzip genshin_character.zip
@@ -8,27 +10,32 @@ current_dir=$(pwd)
 character_dir="$current_dir/genshin_character"
 csv_file="$current_dir/list_character.csv"
 
-for file in "$character_dir"/*.jpg; do
-    decoded_name=$(basename "$file" | xxd -r -p)
 
-    region=$(awk -F',' -v name="$decoded_name" '$1 == name {print $2}' "$csv_file")
-    elemen=$(awk -F',' -v name="$decoded_name" '$1 == name {print $3}' "$csv_file")
-    weapon=$(awk -F',' -v name="$decoded_name" '$1 == name {print $4}' "$csv_file")
-    mkdir -p "$character_dir/$region"
-    
-    if [ -f "$file" ]; then
-        mv "$file" "$character_dir/$region/$region-$decoded_name-$elemen-$weapon.jpg"
+for jpg_file in "$character_dir"/*.jpg; do
+    name_decoded=$(basename "$jpg_file" | xxd -r -p)
+
+
+    char_region=$(awk -F',' -v char_name="$name_decoded" '$1 == char_name {print $2}' "$csv_file")
+    char_elemen=$(awk -F',' -v char_name="$name_decoded" '$1 == char_name {print $3}' "$csv_file")
+    char_weapon=$(awk -F',' -v char_name="$name_decoded" '$1 == char_name {print $4}' "$csv_file")
+    mkdir -p "$character_dir/$char_region"
+   
+    if [ -f "$jpg_file" ]; then
+        mv "$jpg_file" "$character_dir/$char_region/$char_region-$name_decoded-$char_elemen-$char_weapon.jpg"
     else
-        echo "File not found: $file"
+        echo "File not found: $jpg_file"
     fi
 done
 
-weapon_types=(Catalyst Bow Polearm Sword Claymore)
 
-for weapon_type in "${weapon_types[@]}"; do
-  count=$(awk -F',' '/'"$weapon_type"'/ { ++count } END { print count }' "$csv_file")
-  echo "$weapon_type : $count"
+weapon_kinds=(Bow Polearm Claymore Sword Catalyst)
+
+
+for weapon_kind in "${weapon_kinds[@]}"; do
+    weapon_count=$(awk -F',' '/'"$weapon_kind"'/ { ++weapon_count } END { print weapon_count }' "$csv_file")
+    echo "$weapon_kind : $weapon_count"
 done
+
 
 rm genshin.zip
 rm genshin_character.zip
